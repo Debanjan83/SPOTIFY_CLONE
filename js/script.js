59f5a9d86b91942fee9a107995c33c92b2cf235a
Debanjan Pal <debanjanpal79611@gmail.com>
2025-07-20 16:30:25 +0530

    Files

--- js/script.js
+++ js/script.js
@@ -66,7 +66,7 @@ const playMusic = (track, pause = false) => {
 }
 
 async function displayAlbums() {
-    let a = await fetch(`http://127.0.0.1:3000/songs/`)
+    let a = await fetch(`data/songs/`)
     let response = await a.text()
     let div = document.createElement("div")
     div.innerHTML = response
@@ -77,7 +77,7 @@ async function displayAlbums() {
         const e = array[index];
         if (e.href.includes("/songs") && !e.href.includes(".htaccess")) {
             let folder = e.href.split("/").slice(-2)[0]
-            let a = await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`)
+            let a = await fetch(`data/songs/${folder}/info.json`)
             let response = await a.json()
             cardContainer.innerHTML = cardContainer.innerHTML + 
                 `<div data-folder="${folder}" class="card">
